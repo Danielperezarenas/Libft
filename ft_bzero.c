beabdf7dd15a700d36cd7dadb52b84ea75b9bf9c
Danielperezarenas <danierr999@hotmail.com>
2024-10-13 14:32:55 +0200

    add ft_substr, mod ft_calloc and ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -16,6 +16,7 @@ void	ft_bzero(void *s, size_t n)
 {
 	ft_memset(s, 0, n);
 }
+
 /* 
 int	main(void)
 {
--- ft_calloc.c
+++ ft_calloc.c
@@ -6,13 +6,22 @@
 /*   By: danperez <danperez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 20:35:49 by danperez          #+#    #+#             */
-/*   Updated: 2024/10/07 20:55:11 by danperez         ###   ########.fr       */
+/*   Updated: 2024/10/11 04:13:10 by danperez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-	
-} */
+	void	*ptr;
+
+	if (nmemb == 0 || size == 0)
+		ptr = NULL;
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return NULL;
+	ft_bzero(ptr, nmemb * size);
+	return (ptr);
+}
+
